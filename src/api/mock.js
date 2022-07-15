67c973e2daa65d34225e25fdee949fe6a6ad3477
William Stam <willcharlie@Williams-MacBook-Pro.local>
2022-07-15 21:17:12 +0100

    [mock] Add data

---
+++ src/api/mock.js
@@ -0,0 +1,64 @@
+export const logo = ['/images/logo.svg']
+export const iconAngleLeft = ['/images/iconAngleLeft.svg']
+export const iconAngleRight = ['/images/iconAngleRight.svg']
+export const iconArrow = ['/images/iconArrow.svg']
+export const iconClose = ['/images/iconClose.svg']
+export const iconHamburger = ['/images/iconHamburger.svg']
+
+export const sliderImgs = [
+  {
+    img: './images/desktopImageHero-1.jpg'
+  },
+  {
+    img: './images/desktopImageHero-2.jpg'
+  },
+  {
+    img: './images/desktopImageHero-3.jpg'
+  }
+]
+
+export const navigation = [
+  {
+    heading: 'home',
+  },
+  {
+    heading: 'shop'
+  },
+  {
+    heading: 'about'
+  },
+  {
+    heading: 'contact'
+  }
+]
+
+export const heroText = [
+  {
+    heading: `Discover innovative ways to decorate`,
+    text: `We provide unmatched quality, comfort and style for property owners across the country. Our experts combine form and 
+    function in bringing your vision to life. Create a room in your own style with our collection and make your property a 
+    reflection of you and what you love.` 
+  },
+  {
+    heading: `We are available all across the globe`,
+    text: `With stores all over the world, it's easy for you to find furniture for your home or place of business.
+    Locally, we're in most major cities throughout the country. Find the branch nearest you using our store locator. 
+    Any questions? Don't hesitate to contact us today.`
+  },
+  {
+    heading: `Manufactured with the best materials`,
+    text: `Our modern furniture store provide a high level of quality. Our company has invested in advanced technology
+    to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in
+    this industry, we understand what customers want for their home and office.`
+  }
+]
+
+export const subHeroText = [
+  {
+    heading: `About our furniture`,
+    text: `Our multifunctional collection blends design and function to suit your individual taste. Make each room unique,
+    or pick a cohesive theme that best express your interests and what inspires you. Find the furniture pieces you need, 
+    from traditional to contemporary styles or anything in between. Product specialists are available to help you 
+    create your dream space.`
+  }
+]
