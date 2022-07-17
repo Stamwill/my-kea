b09385b2452b6edac73afa227843b86364eb8270
William Stam <williamstam@gmail.com>
2022-07-17 16:35:50 +0100

    [TextContent] Add block for reoccuring text blocks

---
+++ src/blocks/TextContent/TextContent.js
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import classes from './TextContent.module.css'
+import PropTypes from 'prop-types'
+
+const TextContent = React.forwardRef(function TextContent(props, ref) {
+  const { heroText, ...other } = props
+
+  console.log(heroText)
+  return (
+    <section className={classes.root} ref={ref} {...other}>
+      <div className={classes.headingContainer}>
+        <h1 className={classes.heading}>{heroText[0].heading}</h1>
+      </div>
+      <div className={classes.textContainer}>
+        <p className={classes.text}>{heroText[0].text}</p>
+      </div>
+    </section>
+  )
+})
+
+export default TextContent
