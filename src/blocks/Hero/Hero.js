a41fb02a2d004bb4514b068bed1866697653f131
William Stam <williamstam@gmail.com>
2022-07-17 12:50:03 +0100

    [Hero] Create Hero block

---
+++ src/blocks/Hero/Hero.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { sliderImgs } from 'api/mock'
+import classes from './Hero.module.css'
+import PropTypes from 'prop-types'
+
+const Hero = () => {
+  return (
+    <section className={classes.root}>
+      <div>
+        {sliderImgs.map((img, idx) => (
+          <img className={classes.background} src={img.img} alt="test" key={idx} />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Hero
