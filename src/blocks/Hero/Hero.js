763e3aec312733e44fdc07d6e66a01e4822cc51e
William Stam <williamstam@gmail.com>
2022-07-17 21:50:44 +0100

    [Hero] Add arrows and styling

--- src/blocks/Hero/Hero.js
+++ src/blocks/Hero/Hero.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { sliderImgs } from 'api/mock'
+import { sliderImgs, iconAngleLeft, iconAngleRight } from 'api/mock'
 import Picture from 'components/Picture'
 import classes from './Hero.module.css'
 import PropTypes from 'prop-types'
@@ -8,7 +8,11 @@ const Hero = () => {
   return (
     <section className={classes.root}>
       <div className={classes.imgSlider}>
-        <Picture sliderImgs={sliderImgs} />
+        <Picture data={sliderImgs} />
+      </div>
+      <div className={classes.arrowContainer}>
+        <img className={classes.arrow} src={iconAngleLeft} />
+        <img className={classes.arrow} src={iconAngleRight} />
       </div>
     </section>
   )
