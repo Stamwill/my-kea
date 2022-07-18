69d995c477a9c371c38272f3ea6217ce49f7a44f
William Stam <williamstam@gmail.com>
2022-07-18 22:24:39 +0100

    [About] Add data and styling

--- src/blocks/About/About.js
+++ src/blocks/About/About.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { aboutLight, aboutDark } from 'api/mock'
+import { aboutLight, aboutDark, subHeroText } from 'api/mock'
 import classes from './About.module.css'
 import PropTypes from 'prop-types'
 
@@ -10,6 +10,15 @@ const About = React.forwardRef(function About(props, ref) {
     <section className={classes.root} ref={ref} {...other}>
       <div className={classes.imgContainer}>
         <img className={classes.img} src={aboutDark} alt="Light furniture" />
+      </div>
+      {subHeroText.map((text, idx) => (
+        <div className={classes.textContainer} key={idx}>
+          <h3 className={classes.heading}>{text.heading}</h3>
+          <p className={classes.text}>{text.text}</p>
+        </div>
+      ))}
+      <div className={classes.imgContainer}>
+        <img className={classes.img} src={aboutLight} alt="Light furniture" />
       </div>
     </section>
   )
