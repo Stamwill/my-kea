db66bb09c19f4d5cc208d41eecb15b3d447fac63
William Stam <williamstam@gmail.com>
2022-07-19 16:09:20 +0100

    [Hero] Import SlideClicker, Change name on props and change styling

--- src/blocks/Hero/Hero.js
+++ src/blocks/Hero/Hero.js
@@ -1,28 +1,28 @@
 import * as React from 'react'
 import { sliderImgs, iconAngleLeft, iconAngleRight } from 'api/mock'
 import Picture from 'components/Picture'
+import SliderClicker from 'components/SlideClicker'
 import classes from './Hero.module.css'
 import PropTypes from 'prop-types'
 import HeroNav from './partials/HeroNav'
 
 const Hero = (props) => {
-  const { menu, toggleMenu } = props
+  const { open, toggleMenu } = props
   return (
     <section className={classes.root}>
-      <HeroNav menu={menu} toggleMenu={toggleMenu} />
+      <HeroNav open={open} toggleMenu={toggleMenu} />
       <div className={classes.imgSlider}>
         <Picture data={sliderImgs} />
       </div>
-      <div className={classes.arrowContainer}>
-        <img className={classes.arrow} src={iconAngleLeft} />
-        <img className={classes.arrow} src={iconAngleRight} />
+      <div className={classes.clicker}>
+        <SliderClicker />
       </div>
     </section>
   )
 }
 
 Hero.propTypes = {
-  menu: PropTypes.bool,
+  open: PropTypes.bool,
   ToggleMenu: PropTypes.func,
 }
 
