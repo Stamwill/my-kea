e9ab1f875a3be2616e1dac450faba84971bf8d9c
William Stam <williamstam@gmail.com>
2022-07-19 11:47:16 +0100

    [App] Remove AppNav

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -3,14 +3,12 @@ import { heroText } from 'api/mock'
 import Hero from 'blocks/Hero'
 import TextContent from 'blocks/TextContent'
 import classes from './App.module.css'
-import AppNav from './partials/AppNav'
 import About from 'blocks/About'
 
 const App = () => {
   return (
     <div className={classes.root}>
       <header>
-        <AppNav />
         <Hero />
         <TextContent heroText={heroText} />
       </header>
