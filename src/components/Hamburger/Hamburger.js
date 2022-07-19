c1a96e8a5e670803bbd2ce47a6341bead58083f9
William Stam <williamstam@gmail.com>
2022-07-19 13:17:57 +0100

    [Hamburger] Add PropTypes and props

--- src/components/Hamburger/Hamburger.js
+++ src/components/Hamburger/Hamburger.js
@@ -1,13 +1,19 @@
 import * as React from 'react'
 import { iconHamburger } from 'api/mock'
+import PropTypes from 'prop-types'
 import classes from './Hamburger.module.css'
 
-const Hamburger = () => {
+const Hamburger = (props) => {
+  const { toggleMenu } = props
   return (
     <div className={classes.root}>
-      <img className={classes.img} src={iconHamburger} alt="nav menu" />
+      <img className={classes.img} src={iconHamburger} onClick={toggleMenu} alt="nav menu" />
     </div>
   )
+}
+
+Hamburger.propTypes = {
+  toggleMenu: PropTypes.func,
 }
 
 export default Hamburger
