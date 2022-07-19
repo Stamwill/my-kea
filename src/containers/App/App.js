f825ebb426e0d03e23fb1c939441e2087431e7ba
William Stam <williamstam@gmail.com>
2022-07-19 13:19:09 +0100

    [App] Add state and event handler, pass props

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,23 @@
 import * as React from 'react'
 import { heroText } from 'api/mock'
 import Hero from 'blocks/Hero'
+import AppDrawer from './partials/AppDrawer'
 import TextContent from 'blocks/TextContent'
 import classes from './App.module.css'
 import About from 'blocks/About'
 
 const App = () => {
+  const [menuIsOpen, setMenuOpen] = React.useState(false)
+
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState)
+  }
+
   return (
     <div className={classes.root}>
       <header>
-        <Hero />
+        <AppDrawer open={menuIsOpen} toggleMenu={toggleMenu} />
+        <Hero menu={menuIsOpen} toggleMenu={toggleMenu} />
         <TextContent heroText={heroText} />
       </header>
 
