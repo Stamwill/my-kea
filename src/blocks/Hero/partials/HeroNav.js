ce677b7a85d06aafcd4c2a0bcdc2a0a35edae3a9
William Stam <williamstam@gmail.com>
2022-07-19 13:15:50 +0100

    [HeroNav] Add PropTypes and pass props

--- src/blocks/Hero/partials/HeroNav.js
+++ src/blocks/Hero/partials/HeroNav.js
@@ -1,14 +1,15 @@
 import * as React from 'react'
 import { logo } from 'api/mock'
-import Hamburger from 'components/Hamburger/Hamburger'
+import Hamburger from 'components/Hamburger'
+import PropTypes from 'prop-types'
 import classes from './HeroNav.module.css'
 
 const HeroNav = React.forwardRef(function HeroNav(props, ref) {
-  const { ...other } = props
+  const { menu, toggleMenu, ...other } = props
   return (
     <nav className={classes.root} ref={ref} {...other}>
       <div className={classes.hamburgerContainer}>
-        <Hamburger />
+        <Hamburger toggleMenu={toggleMenu} />
       </div>
       <div className={classes.logoContainer}>
         <img className={classes.logo} src={logo} alt="company logo" />
@@ -17,5 +18,10 @@ const HeroNav = React.forwardRef(function HeroNav(props, ref) {
     </nav>
   )
 })
+
+HeroNav.propTypes = {
+  menu: PropTypes.bool,
+  toggleMenu: PropTypes.func,
+}
 
 export default HeroNav
