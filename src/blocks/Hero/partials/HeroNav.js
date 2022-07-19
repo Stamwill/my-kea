efd1f496af26043e156f2689ab566c2bfc2b4ca7
William Stam <williamstam@gmail.com>
2022-07-19 16:08:45 +0100

    [HeroNav] Add navlist and media query

--- src/blocks/Hero/partials/HeroNav.js
+++ src/blocks/Hero/partials/HeroNav.js
@@ -1,11 +1,11 @@
 import * as React from 'react'
-import { logo } from 'api/mock'
+import { logo, navigation } from 'api/mock'
 import Hamburger from 'components/Hamburger'
 import PropTypes from 'prop-types'
 import classes from './HeroNav.module.css'
 
 const HeroNav = React.forwardRef(function HeroNav(props, ref) {
-  const { menu, toggleMenu, ...other } = props
+  const { open, toggleMenu, ...other } = props
   return (
     <nav className={classes.root} ref={ref} {...other}>
       <div className={classes.hamburgerContainer}>
@@ -13,6 +13,15 @@ const HeroNav = React.forwardRef(function HeroNav(props, ref) {
       </div>
       <div className={classes.logoContainer}>
         <img className={classes.logo} src={logo} alt="company logo" />
+      </div>
+      <div className={classes.navList}>
+        {navigation.map((item, idx) => (
+          <div key={idx}>
+            <a className={classes.navItem} href="#">
+              {item.heading}
+            </a>
+          </div>
+        ))}
       </div>
       <span className={classes.hidden}>hidden</span>
     </nav>
