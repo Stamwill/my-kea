7b9c746ac28b7a2d22f8d8a5475290b5729a1bb7
William Stam <williamstam@gmail.com>
2022-07-19 11:46:03 +0100

    [Hero] Add HeroNav and add styling

--- src/blocks/Hero/Hero.js
+++ src/blocks/Hero/Hero.js
@@ -3,17 +3,19 @@ import { sliderImgs, iconAngleLeft, iconAngleRight } from 'api/mock'
 import Picture from 'components/Picture'
 import classes from './Hero.module.css'
 import PropTypes from 'prop-types'
+import HeroNav from './partials/HeroNav'
 
 const Hero = () => {
   return (
     <section className={classes.root}>
+      <HeroNav />
       <div className={classes.imgSlider}>
         {/* <Picture data={sliderImgs} /> */}
-        <img className={classes.imgTest} src={sliderImgs[0].background.mobile} />
-        <div className={classes.arrowContainer}>
-          <img className={classes.arrow} src={iconAngleLeft} />
-          <img className={classes.arrow} src={iconAngleRight} />
-        </div>
+        <img className={classes.img} src={sliderImgs[0].background.mobile} />
+      </div>
+      <div className={classes.arrowContainer}>
+        <img className={classes.arrow} src={iconAngleLeft} />
+        <img className={classes.arrow} src={iconAngleRight} />
       </div>
     </section>
   )
