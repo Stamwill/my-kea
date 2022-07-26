6aba94d567d6fc45e751a5e98d337e7b12fcd556
William Stam <williamstam@gmail.com>
2022-07-26 16:02:35 +0100

    [SliderClicker] Add PropTypes

--- src/components/SlideClicker/SliderClicker.js
+++ src/components/SlideClicker/SliderClicker.js
@@ -1,15 +1,20 @@
 import * as React from 'react'
 import { iconAngleLeft, iconAngleRight } from 'api/mock'
+import PropTypes from 'prop-types'
 import classes from './SliderClicker.module.css'
 
 const SliderClicker = (props) => {
-  const {} = props
+  const { toggleSlide } = props
   return (
     <div className={classes.root}>
-      <img className={classes.arrow} src={iconAngleLeft} />
+      <img className={classes.arrow} src={iconAngleLeft} onClick={toggleSlide} />
       <img className={classes.arrow} src={iconAngleRight} />
     </div>
   )
+}
+
+SliderClicker.propTypes = {
+  toggleSlide: PropTypes.func,
 }
 
 export default SliderClicker
