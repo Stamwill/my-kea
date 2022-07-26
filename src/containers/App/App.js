423171719f61fecb8766c32ffd2f95fc390e8aa0
William Stam <williamstam@gmail.com>
2022-07-26 16:02:58 +0100

    [App] Add temporary state and slide handler

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -8,6 +8,13 @@ import About from 'blocks/About'
 
 const App = () => {
   const [menuIsOpen, setMenuOpen] = React.useState(false)
+  const [heading, setSlider] = React.useState(heroText[0].heading)
+  const [text, setText] = React.useState(heroText[0].text)
+
+  const toggleSlide = () => {
+    setSlider(heroText[1].heading)
+    setText(heroText[1].text)
+  }
 
   const toggleMenu = () => {
     setMenuOpen((prevState) => !prevState)
@@ -18,7 +25,7 @@ const App = () => {
       <header className={classes.header}>
         <AppDrawer open={menuIsOpen} toggleMenu={toggleMenu} />
         <Hero toggleMenu={toggleMenu} />
-        <TextContent heroText={heroText} />
+        <TextContent heading={heading} text={text} toggleSlide={toggleSlide} />
       </header>
 
       <main>
